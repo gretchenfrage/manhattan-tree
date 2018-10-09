2435864f7e134358828ddf2304d63cc2cbaff289
Phoenix <kahlo.phoenix@gmail.com>
2018-10-08 21:01:09 -0400

    possibly get insertion into a correctly working state

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate num;
 extern crate take_mut;
 
 use std::collections::VecDeque;
-use std::cmp::min;
-use std::mem;
+use std::cmp::max;
+use std::fmt::{Debug, Formatter};
+use std::fmt;
 
 use take_mut::take;
 use num::Integer;
@@ -19,7 +20,7 @@ use num::Integer;
 ///
 /// Currently, the coord is hard-coded to use 64-bit coordinates and three dimensions. Genericity
 /// with regard to these properties is a prime candidite for a future feaature.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub struct OctCoord {
     base: BaseCoord,
     scale: u8
@@ -28,7 +29,7 @@ impl OctCoord {
     /// Given a base coord, determine which suboctant value denotes the child of this oct coord
     /// which the base coord would fit into, or none if the coord is not a valid child of this
     /// coord.
-    fn suboctant_base(self, coord: BaseCoord) -> Option<SubOctant> {
+    fn suboctant(self, coord: BaseCoord) -> Option<SubOctant> {
         // for it to be a valid child, the bits past scale magnitude must be equal
         if
             childnt(self.base.comps[0], self.scale, coord.comps[0]) ||
@@ -44,6 +45,20 @@ impl OctCoord {
             Some([x, y, z])
         }
     }
+
+    fn to_base(self) -> BaseCoord {
+        let x = self.base.comps[0] << self.scale;
+        let y = self.base.comps[1] << self.scale;
+        let z = self.base.comps[2] << self.scale;
+        BaseCoord {
+            comps: [x, y, z]
+        }
+    }
+}
+impl Debug for OctCoord {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        f.write_str(&format!("{:?}*2^{}", self.base, self.scale))
+    }
 }
 
 /// Helper function for OctCoord::suboctant_base.
@@ -53,7 +68,7 @@ fn childnt(scaled_comp: u64, scale: u8, base_comp: u64) -> bool {
 
 /// The component of the coord which is not scaled. When this type is used on its own, it
 /// generally refers to a coord with a scale factor of 1.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub struct BaseCoord {
     comps: [u64; 3]
 }
@@ -68,13 +83,13 @@ impl BaseCoord {
         let (z_comp, z_scale) = commonize_component(self.comps[2], other.comps[2]);
 
         // find the minimum scale, which will become to oct coord scale
-        let scale = min(min(x_scale, y_scale), z_scale);
+        let scale = max(max(x_scale, y_scale), z_scale);
 
         // increase the magnitude of the components to maintain their equality after transitioning
         // them to the possibly smaller scale
-        let x = x_comp << (x_scale - scale);
-        let y = y_comp << (y_scale - scale);
-        let z = z_comp << (z_scale - scale);
+        let x = x_comp >> (scale - x_scale);
+        let y = y_comp >> (scale - y_scale);
+        let z = z_comp >> (scale - z_scale);
 
         // done
         OctCoord {
@@ -82,6 +97,18 @@ impl BaseCoord {
                 comps: [x, y, z],
             },
             scale
+        }
+    }
+}
+impl Debug for BaseCoord {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        f.write_str(&format!("[{},{},{}]", self.comps[0], self.comps[1], self.comps[2]))
+    }
+}
+impl From<[u64; 3]> for BaseCoord {
+    fn from(comps: [u64; 3]) -> Self {
+        BaseCoord {
+            comps
         }
     }
 }
@@ -104,6 +131,7 @@ pub enum Pole {
     N,
 }
 impl Pole {
+    #[allow(dead_code)]
     fn from_delta<I: Integer>(delta: I) -> Option<Pole> {
         if delta > I::zero() {
             Some(Pole::P)
@@ -129,6 +157,7 @@ impl Pole {
 pub type SubOctant = [Pole; 3];
 
 /// A group of 8 items, identified by suboctant.
+#[derive(Debug)]
 pub struct Children<T> {
     ppp: T,
     npp: T,
@@ -142,22 +171,23 @@ pub struct Children<T> {
 impl<T> Children<T> {
     fn new(mut gen: impl FnMut(SubOctant) -> T) -> Self {
         Children {
-            ppp: default([Pole::P, Pole::P, Pole::P]),
-            npp: default([Pole::N, Pole::P, Pole::P]),
-            pnp: default([Pole::P, Pole::N, Pole::P]),
-            ppn: default([Pole::P, Pole::P, Pole::N]),
-            pnn: default([Pole::P, Pole::N, Pole::N]),
-            npn: default([Pole::N, Pole::P, Pole::N]),
-            nnp: default([Pole::N, Pole::N, Pole::P]),
-            nnn: default([Pole::N, Pole::N, Pole::N]),
+            ppp: gen([Pole::P, Pole::P, Pole::P]),
+            npp: gen([Pole::N, Pole::P, Pole::P]),
+            pnp: gen([Pole::P, Pole::N, Pole::P]),
+            ppn: gen([Pole::P, Pole::P, Pole::N]),
+            pnn: gen([Pole::P, Pole::N, Pole::N]),
+            npn: gen([Pole::N, Pole::P, Pole::N]),
+            nnp: gen([Pole::N, Pole::N, Pole::P]),
+            nnn: gen([Pole::N, Pole::N, Pole::N]),
         }
     }
 
+    #[allow(dead_code)]
     fn get(&self, key: SubOctant) -> &T {
         match key {
             [Pole::P, Pole::P, Pole::P] => &self.ppp,
             [Pole::N, Pole::P, Pole::P] => &self.npp,
-            [Pole::P, Pole::N, Pole::P] => &self.npn,
+            [Pole::P, Pole::N, Pole::P] => &self.pnp,
             [Pole::P, Pole::P, Pole::N] => &self.ppn,
             [Pole::P, Pole::N, Pole::N] => &self.pnn,
             [Pole::N, Pole::P, Pole::N] => &self.npn,
@@ -170,7 +200,7 @@ impl<T> Children<T> {
         match key {
             [Pole::P, Pole::P, Pole::P] => &mut self.ppp,
             [Pole::N, Pole::P, Pole::P] => &mut self.npp,
-            [Pole::P, Pole::N, Pole::P] => &mut self.npn,
+            [Pole::P, Pole::N, Pole::P] => &mut self.pnp,
             [Pole::P, Pole::P, Pole::N] => &mut self.ppn,
             [Pole::P, Pole::N, Pole::N] => &mut self.pnn,
             [Pole::N, Pole::P, Pole::N] => &mut self.npn,
@@ -181,6 +211,7 @@ impl<T> Children<T> {
 }
 
 /// Ahe manhattan tree.
+#[derive(Debug)]
 pub struct Tree<T> {
     root: Option<Octant<T>>
 }
@@ -191,15 +222,17 @@ impl<T> Tree<T> {
         }
     }
 
-    pub fn add(&mut self, coord: BaseCoord, elem: T) {
+    pub fn add(&mut self, coord: impl Into<BaseCoord>, elem: T) {
+        let coord = coord.into();
         if let Some(ref mut root) = self.root {
             root.add(coord, elem);
         } else {
-            self.root = Some(Octant::leaf_of(coord, elems));
+            self.root = Some(Octant::leaf_of(coord, elem));
         }
     }
 
-    pub fn take(&mut self, focus: BaseCoord) -> Option<T> {
+    pub fn take(&mut self, focus: impl Into<BaseCoord>) -> Option<T> {
+        let focus = focus.into();
         let (elem, emptied) =
             if let Some(ref mut root) = self.root {
                 let (elem, emptied) = root.take(focus);
@@ -219,13 +252,14 @@ impl<T> Tree<T> {
 }
 
 /// An octant in the manhattan tree.
+#[derive(Debug)]
 enum Octant<T> {
     Leaf {
         coord: BaseCoord,
         elems: VecDeque<T>, // TODO: store in-place, with genericity over array size
     },
     Branch {
-        center: OctCoord,
+        coord: OctCoord,
         children: Children<Box<Octant<T>>>,
     },
     Empty,
@@ -243,6 +277,7 @@ impl<T> Octant<T> {
 
     /// Does not assume that the coordinate is part of this octant's range
     fn add(&mut self, elem_coord: BaseCoord, elem: T) {
+        // replace self through pattern matching
         take(self, |octant| match octant {
             Octant::Leaf {
                 coord: leaf_coord,
@@ -262,14 +297,16 @@ impl<T> Octant<T> {
                     // branch at the smallest common octant
                     let branch_coord = elem_coord.lowest_common_octant(leaf_coord);
 
-                    let old_suboctant = branch_coord.suboctant_base(leaf_coord);
+                    println!("branching leaves {:?} and {:?} at {:?}", elem_coord, leaf_coord, branch_coord);
+
+                    let old_suboctant = branch_coord.suboctant(leaf_coord).unwrap();
                     let old_child = Octant::Leaf {
                         coord: leaf_coord,
                         elems: leaf_elems
                     };
                     let mut old_child = Some(old_child);
 
-                    let new_suboctant = branch_coord.suboctant_base(elem_coord);
+                    let new_suboctant = branch_coord.suboctant(elem_coord).unwrap();
                     let new_child = Octant::leaf_of(elem_coord, elem);
                     let mut new_child = Some(new_child);
 
@@ -284,37 +321,92 @@ impl<T> Octant<T> {
                     });
 
                     Octant::Branch {
-                        center: branch_coord,
+                        coord: branch_coord,
                         children
                     }
                 }
             },
             Octant::Branch {
-                center: branch_center,
-                children: mut branch_children
+                coord: branch_coord,
+                children: mut branch_children,
             } => {
                 // case 2: we're a branch
-                if let Some(child) = branch_center.suboctant_base(elem_coord) {
+                if let Some(child) = branch_coord.suboctant(elem_coord) {
                     // case 2a: the new element is a child of this branch
                     // simply add to the appropriate child
                     branch_children.get_mut(child).add(elem_coord, elem);
+                    Octant::Branch {
+                        coord: branch_coord,
+                        children: branch_children,
+                    }
                 } else {
                     // case 2b: the new element is not a child of this branch
-                    // we need to expand the branch center to a new lowest common octant of children
-                    
-                }
+                    // in this case, we need to produce a new super branch
 
+                    let old_branch_coord = branch_coord;
+                    let new_branch_coord = old_branch_coord.to_base()
+                        .lowest_common_octant(elem_coord);
+
+                    let old_suboctant =
+                        new_branch_coord.suboctant(old_branch_coord.to_base()).unwrap();
+                    let old_child = Octant::Branch {
+                        coord: old_branch_coord,
+                        children: branch_children
+                    };
+                    let mut old_child = Some(old_child);
+
+                    let new_suboctant = new_branch_coord.suboctant(elem_coord).unwrap();
+                    let new_child = Octant::leaf_of(elem_coord, elem);
+                    let mut new_child = Some(new_child);
+
+                    let children = Children::new(|suboctant| {
+                        if suboctant == old_suboctant {
+                            Box::new(old_child.take().unwrap())
+                        } else if suboctant == new_suboctant {
+                            Box::new(new_child.take().unwrap())
+                        } else {
+                            Box::new(Octant::Empty)
+                        }
+                    });
+
+                    Octant::Branch {
+                        coord: new_branch_coord,
+                        children
+                    }
+                }
             },
+            Octant::Empty => {
+                // case 3: we're empty
+                // simply become a single-element leaf
+                Octant::leaf_of(elem_coord, elem)
+            }
         })
     }
 
     /// Take an element, and return whether this octant has become empty, and therefore
     /// invalid and in need of deletion.
-    pub fn take(&mut self, focus: BaseCoord) -> (T, bool) {
+    pub fn take(&mut self, _focus: BaseCoord) -> (T, bool) {
         unimplemented!()
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut tree = Tree::new();
+
+    /*
+    tree.add([0, 0, 0], "a");
+    tree.add([0, 0, 0], "a2");
+    tree.add([1, 1, 1], "b");
+    */
+
+    for x in 0..2 {
+        for y in 0..3 {
+            for z in 0..5 {
+                println!("adding {}{}{}", z, y, z);
+                tree.add([x, y, z], format!("{}{}{}", x, y, z));
+            }
+        }
+    }
+
+    println!("{:#?}", tree);
 }
